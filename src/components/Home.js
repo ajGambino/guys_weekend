b5f7bccc486b61eb7931d16d6c4d1cbe9ac54847
AJ Gambino <66279789+ajGambino@users.noreply.github.com>
2024-06-27 03:10:02 -0400

    fix auto deploys

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const [players, setPlayers] = useState([]);
 
-export default Home
+    useEffect(() => {
+        // Fetch players from Firebase Firestore
+        const fetchPlayers = async () => {
+            try {
+                const snapshot = await firebase.firestore().collection('players').get();
+                const playerData = snapshot.docs.map((doc) => doc.data());
+                setPlayers(playerData);
+            } catch (error) {
+                console.error('Error fetching players:', error);
+            }
+        };
+
+        fetchPlayers();
+    }, []);
+
+    return (
+        <div className='home-page'>
+            <h1>GW '24</h1>
+            {players.length === 0 ? (
+                <p>No players available.</p>
+            ) : (
+                <table className='stats'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Net</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {players.map((player) => (
+                            <tr key={player.name}>
+                                <td className='name-cell'>{player.name}</td>
+                                <td className='net-cell' >${player.net}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}</div>
+
+    );
+};
+
+export default Home;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
 
-export default Login
+    const handleGoogleLogin = async () => {
+        try {
+            const provider = new firebase.auth.GoogleAuthProvider();
+            await firebase.auth().signInWithPopup(provider);
+
+            navigate('/home');
+        } catch (error) {
+            setError('Failed to log in with Google');
+            console.error('Error logging in with Google:', error);
+        }
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        try {
+            // Sign in the user with email and password
+            await firebase.auth().signInWithEmailAndPassword(email, password);
+
+            // Redirect to the landing page or any other protected route
+            navigate('/home');
+        } catch (error) {
+            setError('Failed to log in');
+            console.error('Error logging in:', error);
+        }
+    };
+
+    return (
+        <div className='login'>
+            <h1>Login</h1>
+            <button type="button" onClick={handleGoogleLogin}>
+                Login with Google
+            </button> <br></br>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" value={email} onChange={handleEmailChange} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+                </div><br></br>
+                <button type="submit">Login</button>
+            </form>
+
+            {/*  <p>
+                Don't have an account? <Link to="/signup">Sign up now</Link>
+    </p> */}
+        </div>
+    );
+};
+
+export default Login;
