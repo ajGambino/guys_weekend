72fe1fd6cfc605d3b24abc8d82f7bce252dc6253
AJ Gambino <66279789+ajGambino@users.noreply.github.com>
2024-07-01 22:13:17 -0400

    button styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -66,11 +66,33 @@ const Login = () => {
         }
     };
 
+    const GoogleLogo = () => (
+        <svg viewBox="0 0 48 48" width="24px" height="24px">
+          <path
+            fill="#4285F4"
+            d="M24 9.5c3.8 0 6.2 1.6 7.6 2.9l5.6-5.6C33.4 3.6 29.1 2 24 2 14.8 2 7.4 7.8 5 15.4l6.9 5.4C13.6 13 18.3 9.5 24 9.5z"
+          />
+          <path
+            fill="#34A853"
+            d="M46.5 24c0-1.5-.1-2.9-.3-4.3H24v8.3h12.8c-.6 3.3-2.6 6.1-5.5 8l6.9 5.4C43.1 37.8 46.5 31.4 46.5 24z"
+          />
+          <path
+            fill="#FBBC05"
+            d="M12.6 28.7c-1.6-2.4-2.5-5.1-2.5-8s.9-5.6 2.5-8l-6.9-5.4C2.8 11.5 0 17.4 0 24s2.8 12.5 7.7 16.7l6.9-5.4z"
+          />
+          <path
+            fill="#EA4335"
+            d="M24 46c6.2 0 11.4-2.1 15.2-5.6l-6.9-5.4c-2.1 1.4-4.8 2.3-8.3 2.3-5.6 0-10.3-3.5-12-8.4l-6.9 5.4C7.4 40.2 14.8 46 24 46z"
+          />
+        </svg>
+      );
+      
+
     return (
         <div className='login'>
             <img src={login}/>
-            <button type="button" onClick={handleGoogleLogin}>
-                Sign in with Google
+            <button className="google-sign-in" type="button" onClick={handleGoogleLogin}>
+              <GoogleLogo /> <span>Sign in with Google</span>
             </button>
            
             {error && <p>{error}</p>}
@@ -84,7 +106,7 @@ const Login = () => {
                     <input type="password" id="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <br />
-                <button type="submit">Sign in</button>
+                <button className="login-btn" type="submit">Sign in</button>
             </form>
         </div>
     );
